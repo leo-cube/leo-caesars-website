59dd7b10b7e3f29a1b7b8a62277c32ad34621410
Cube <82458558+leo-cube@users.noreply.github.com>
2025-07-28 23:24:25 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import tagger from '@dhiwise/component-tagger';
+
 export default defineConfig({
   base: './',
   plugins: [tsconfigPaths(), react(), tagger()],
   build: {
-    outDir: 'dist', // Output build folder
+    outDir: 'dist',
     chunkSizeWarningLimit: 2000
   },
   server: {
