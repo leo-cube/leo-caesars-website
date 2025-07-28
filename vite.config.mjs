a00f28017527f23b50947f4d06677f06db35dc16
Cube <82458558+leo-cube@users.noreply.github.com>
2025-07-28 22:39:38 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,8 @@ import tagger from "@dhiwise/component-tagger";
 export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
-  base: '/',
+  base: './',
+  plugins: [react()],
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 2000,
